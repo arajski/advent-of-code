f3a8e73bee860e7928f9fc7763f87ae23f9fd032
Artur Rajski <artur.rajski@hotmail.com>
2022-12-05 09:23:25 +1100

    refactor day1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,36 +1,11 @@
 use std::fs::read_to_string;
 use std::str::FromStr;
 
-#[derive(PartialEq, Eq, Debug, Clone)]
+#[derive(PartialEq, Eq, Clone, Copy)]
 enum Play {
     Rock = 1,
     Paper,
     Scissors
-}
-
-fn parse_round(line: &str) -> i32 {
-    let (op_play, my_play) = line.split_once(" ")
-        .expect("To have 2 plays");
-    return parse_play(op_play, my_play);
-}
-
-fn parse_play(opponent: &str, mine: &str) -> i32 {
-    let rules = [Play::Rock, Play::Paper, Play::Scissors];
-    let op_play = Play::from_str(opponent).expect("To be something");
-    let my_play = Play::from_str(mine).expect("To be someting");
-
-    let op_idx = rules.iter().position(|x| x == &op_play).expect("To be an index");
-    let my_idx = rules.iter().position(|x| x == &my_play).expect("To be an index");
-
-    if op_idx == my_idx {
-        return my_play as i32 + 3; 
-    }
-    else if my_idx == ((op_idx+1) % 3) {
-        return my_play as i32  + 6;
-    }
-    else {
-        return my_play as i32 + 0;
-    }
 }
 
 impl FromStr for Play {
@@ -43,6 +18,27 @@ impl FromStr for Play {
             "C"|"Z" => Ok(Play::Scissors),
             _ => Err(())
         }
+    }
+}
+
+fn parse_round(line: &str) -> i32 {
+    let (op_play, my_play) = line.split_once(" ")
+        .expect("To have 2 plays");
+    return parse_play(op_play, my_play);
+}
+
+fn parse_play(opponent: &str, mine: &str) -> i32 {
+    let op_play = Play::from_str(opponent).expect("To be something") as i32;
+    let my_play = Play::from_str(mine).expect("To be someting") as i32;
+
+    if op_play == my_play {
+        return my_play + 3; 
+    }
+    else if my_play -1 == op_play % 3 {
+        return my_play + 6;
+    }
+    else {
+        return my_play + 0;
     }
 }
 
