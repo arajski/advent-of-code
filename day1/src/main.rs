29123d4a740ebb880c8237e4306b4b6b7ed8acef
Artur Rajski <artur.rajski@hotmail.com>
2022-12-04 20:40:13 +1100

    part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,18 +1,24 @@
 use std::fs::read_to_string;
 
+
 fn sum_calories(calories: &str) -> i32 {
     let cals = calories.lines().map(|cal| {cal.parse::<i32>().unwrap_or(0)}).sum();
     return cals;
 }
 
 fn main() {
-    let result = read_to_string("input.txt").expect("To have content").as_str()
+    let mut result: Vec<i32> = read_to_string("input.txt")
+        .expect("To have content")
+        .as_str()
     .split("\n\n")
     .map(sum_calories)
-        .max();
+        .collect();
 
-    match result {
-        Some(cals) => println!("Most calories: {cals}"),
-        None => println!("Oops!"),
-    }
+    result.sort();
+
+    let part1: i32 = result.to_owned().into_iter().rev().take(1).sum();
+    let part2: i32 = result.to_owned().into_iter().rev().take(3).sum();
+
+    println!("{:?}", part1);
+    println!("{:?}", part2);
 }
