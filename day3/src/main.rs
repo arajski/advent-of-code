823c3b0abab53be8f9dd7f86b8d98b4d1c8662ce
Artur Rajski <artur.rajski@hotmail.com>
2022-12-08 09:25:05 +1100

    refactor

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,33 +1,46 @@
 use std::fs::read_to_string;
 
-fn get_priority(character: char) -> i32 {
-    if character.is_uppercase() {
-        return character as i32 - 38;
-    }
-    return character as i32 - 96;
+trait Priority {
+    type Err;
+    fn into_priority(&self) -> Result<i32, Self::Err>;
 }
 
-fn parse_items(line: &str) -> i32 {
-    let (first_part, second_part) = line.split_at(line.chars().count()/2);
-    let mut priorities: Vec<i32> = Vec::new();
+impl Priority for char {
+    type Err = ();
+    fn into_priority(&self) -> Result<i32, Self::Err> {
+        let ascii = *self as i32;
 
-    for c in first_part.chars() {
-        if second_part.contains(c) {
-            let priority = get_priority(c);
-            if !priorities.contains(&priority) {
-                priorities.push(priority);
-            }
-
+        match ascii {
+            65..=90 => Ok(ascii - 38),
+            97..=122 => Ok(ascii - 96),
+            _ => Err(())
         }
     }
 
-    return priorities.iter().sum();
+}
+
+fn part1(line: &str) -> i32 {
+    let (first_part, second_part) = line.split_at(line.chars().count()/2);
+
+
+    let mut result: Vec<char> = first_part
+        .chars()
+        .filter(|&x| second_part.contains(x))
+        .collect();
+
+    result.sort();
+    result.dedup();
+    
+    return result
+        .iter()
+        .map(|c| char::into_priority(&c).expect("to be a priority"))
+        .sum();
 }
 
 fn main() {
     let result: i32 = read_to_string("input.txt").expect("To have stuff")
         .lines()
-        .map(parse_items)
+        .map(part1)
         .sum();
 
     println!("{:?}", result);
