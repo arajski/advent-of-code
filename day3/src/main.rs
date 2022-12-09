3dc8b14acc9f72dac758461dc1f7f05e4244d0fc
Artur Rajski <artur.rajski@hotmail.com>
2022-12-09 11:48:02 +1100

    day3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -22,7 +22,6 @@ impl Priority for char {
 fn part1(line: &str) -> i32 {
     let (first_part, second_part) = line.split_at(line.chars().count()/2);
 
-
     let mut result: Vec<char> = first_part
         .chars()
         .filter(|&x| second_part.contains(x))
@@ -37,11 +36,39 @@ fn part1(line: &str) -> i32 {
         .sum();
 }
 
+fn part2(parta: &str, partb: &str, partc: &str)-> i32 {
+    let mut result: Vec<char> = parta
+        .chars()
+        .filter(|&x| partb.contains(x) && partc.contains(x))
+        .collect();
+
+    result.sort();
+    result.dedup();
+
+    return result.iter().map(|c| char::into_priority(&c).expect("to be a priority")).sum()
+}
+
 fn main() {
-    let result: i32 = read_to_string("input.txt").expect("To have stuff")
+    let part1_result: i32 = read_to_string("input.txt").expect("To have stuff")
         .lines()
         .map(part1)
         .sum();
 
-    println!("{:?}", result);
+    let part2_input = read_to_string("input.txt").expect("To have stuff");
+    let mut lines = part2_input.lines();
+
+    let mut part2_sum = 0;
+    while let(
+    Some(parta), 
+    Some(partb), 
+    Some(partc)) = (
+        lines.next(), 
+        lines.next(), 
+        lines.next()) {
+        part2_sum = part2_sum + part2(parta, partb, partc);
+            
+    }
+
+    println!("{:?}", part1_result);
+    println!("{:?}", part2_sum);
 }
